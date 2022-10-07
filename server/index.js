08d805d43b7093953699c2006ce656565f479beb
Adrien Lavisiera <alavisiera@veepee.com>
2022-10-07 11:46:54 +0200

    Add host

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const io = new Server(httpServer, {
       "http://localhost:1234",
       "https://blockchat-space.netlify.app",
       "http://127.0.0.1:5500",
+      "https://meepo-chat.surge.sh",
     ],
     credentials: true,
   },
